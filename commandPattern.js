86e1c4a161a3288ecf76b93269b6bc13c09b7602
Abulkosim <abulkasimkattaev@gmail.com>
2025-02-18 00:17:11 +0500

    command pattern

--- commandPattern.js
+++ commandPattern.js
@@ -1,19 +1,62 @@
+// class OrderManager {
+//     constructor() {
+//         this.orders = []
+//     }
+
+//     placeOrder(order, id) {
+//         this.orders.push(id)
+//         return `You have successfully ordered ${order} (${id})`;
+//     }
+
+//     trackOrder(id) {
+//         return `Your order ${id} will arrive in 20 minutes.`
+//     }
+
+//     cancelOrder(id) {
+//         this.orders = this.orders.filter(order => order.id !== id)
+//         return `You have canceled your order ${id}`
+//     }
+// }
+
 class OrderManager {
     constructor() {
-        this.orders = []
+      this.orders = [];
     }
-
-    placeOrder(order, id) {
-        this.orders.push(id)
-        return `You have successfully ordered ${order} (${id})`;
+  
+    execute(command, ...args) {
+      return command.execute(this.orders, ...args);
     }
-
-    trackOrder(id) {
-        return `Your order ${id} will arrive in 20 minutes.`
+  }
+  
+  class Command {
+    constructor(execute) {
+      this.execute = execute;
     }
-
-    cancelOrder(id) {
-        this.orders = this.orders.filter(order => order.id !== id)
-        return `You have canceled your order ${id}`
-    }
-}
+  }
+  
+  function PlaceOrderCommand(order, id) {
+    return new Command(orders => {
+      orders.push(id);
+      console.log(`You have successfully ordered ${order} (${id})`);
+    });
+  }
+  
+  function CancelOrderCommand(id) {
+    return new Command(orders => {
+      orders = orders.filter(order => order.id !== id);
+      console.log(`You have canceled your order ${id}`);
+    });
+  }
+  
+  function TrackOrderCommand(id) {
+    return new Command(() =>
+      console.log(`Your order ${id} will arrive in 20 minutes.`)
+    );
+  }
+  
+  const manager = new OrderManager();
+  
+  manager.execute(new PlaceOrderCommand("Pad Thai", "1234"));
+  manager.execute(new TrackOrderCommand("1234"));
+  manager.execute(new CancelOrderCommand("1234"));
+  
